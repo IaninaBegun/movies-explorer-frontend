f5fa26b18837329fd00eb33891919dacf09d5d53
Ianina Begun <ianina.begun@yandex.ru>
2021-05-13 05:38:43 +0300

    more bugfixes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -20,10 +20,17 @@ function Navigation () {
 
     <nav className={ !isOpen ? `header__navigation header__navigation_burger_closed` : `header__navigation_burger_opened` }>
       <button className={ isOpen ? `header__nav-btn_close` : `header__nav-btn ` } onClick={()=>openMenu()}/>
-      <Link to="/" className={ isOpen ? `header__nav-link` : `header__logo-link_inactive` }>Главная</Link>
-      <Link to="/movies" className="header__nav-link header__nav-link_movies">Фильмы</Link>
-      <Link to="/saved-movies" className="header__nav-link header__nav-link_saved-movies">Сохранённые фильмы</Link>
-      <Link to="/profile" className="header__nav-link header__nav-link_profile">
+      <Link to="/" className={ isOpen ?
+        `header__nav-link` : `header__logo-link_inactive` }>Главная</Link>
+
+      <Link to="/movies" className={ !isOpen ?
+         `header__nav-link header__nav-link_inactive` : `header__nav-link_movies` }
+      >Фильмы</Link>
+      <Link to="/saved-movies" className= { !isOpen ?
+         `header__nav-link header__nav-link_inactive` : `header__nav-link_saved-movies`}
+      >Сохранённые фильмы</Link>
+      <Link to="/profile" className= { !isOpen ?
+         `header__nav-link header__nav-link_inactive` : `header__nav-link_profile` }>
         <>
           <p className="header__profile-text">Аккаунт</p>
           <img src={profileLogo} alt="Иконка ссылки на профиль пользователя" className="header__profile-img"></img>
